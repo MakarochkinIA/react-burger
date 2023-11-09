534ac8dd4d36cf52f54d36506328785128ff3574
Ilya <ilya.makarochkin@yandex.ru>
2023-11-09 22:59:00 +0500

    cypress login

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,23 @@
 describe('service is available', function() {
   it('should be available on localhost:3000', function() {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3001');
+  });
+});
+
+describe('token testing', function() {
+  beforeEach(() => {
+    window.localStorage.setItem(
+      'refreshToken',
+      JSON.stringify('test-refreshToken')
+    )
+    window.localStorage.setItem(
+      'accessToken',
+      JSON.stringify('test-accessToken')
+    )
+    cy.intercept('https://norma.nomoreparties.space/api/auth/user', {fixture: 'user'}).as('fetchUser')
+    cy.visit('http://localhost:3001');
+  })
+  it('should be available on localhost:3000', function() {
+    cy.get('.app-header_right_item__OSYet').first().click()
   });
 });
